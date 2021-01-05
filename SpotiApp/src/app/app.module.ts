cab81eb120c747a117bdb16d0cacad4ce4a01ee0
Andres Bardales <andresbardales15@gmail.com>
2021-01-04 20:15:21 -0600

    Generando componentes home y search

--- SpotiApp/src/app/app.module.ts
+++ SpotiApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
